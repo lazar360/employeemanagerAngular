4b74bbaa923121437eb7f4149e6e329bd0f8596a
NicolasGautier <nicolas.gautier35135@gmail.com>
2022-04-25 19:37:01 +0200

    add modals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Employee } from './employee';
 import { EmployeeService } from './employee.service';
 
@@ -10,6 +11,7 @@ import { EmployeeService } from './employee.service';
 })
 export class AppComponent implements OnInit {
   public employees!: Employee[];
+  public editEmployee: Employee|null = null;
 
   constructor(private employeeService: EmployeeService) {}
 
@@ -27,5 +29,54 @@ export class AppComponent implements OnInit {
       }
     );
   }
+
+public onAddEmployee(addForm: NgForm): void {
+  document.getElementById('add-employee-form')?.click();//close the modal
+  this.employeeService.addEmployee(addForm.value).subscribe(
+    (response: Employee) => {
+      console.log(response);
+      this.getEmployees();
+     },
+    (error: HttpErrorResponse) => {
+      alert(error.message);
+      }
+  );
+}
+
+public onUpdateEmployee(employee: Employee): void {
+  this.employeeService.addEmployee(employee).subscribe(
+    (response: Employee) => {
+      console.log(response);
+      this.getEmployees();
+     },
+    (error: HttpErrorResponse) => {
+      alert(error.message);
+      }
+  );
+}
+
+public onOpenModal(employee: Employee|null, mode: string): void {
+const container = document.getElementById('main-container'); 
+const button = document.createElement('button');
+ button.type = 'button';
+ button.style.display = 'none';
+ button.setAttribute('data-toggle', 'modal');
+
+ if(mode === 'add') {
+   button.setAttribute('data-target', '#addEmployeeModal');
+ }
+
+ if(mode === 'edit') {
+   this.editEmployee = employee;
+  button.setAttribute('data-target', '#updateEmployeeModal');
+}
+
+if(mode === 'delete') {
+  button.setAttribute('data-target', '#deleteEmployeeModal');
+}
+  container?.appendChild(button);
+  button.click();
+
+}
 
 }
